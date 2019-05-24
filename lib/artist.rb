1b4c0321a6c93e1870bf66ce95a6ba2077b55bec
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-24 03:32:50 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,36 @@
+class Artist
+  attr_reader :name
+
+  attr_accessor :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    @songs << song
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def genres
+    Genre.all.select {|genre| genre.artist = self}
+  end
+
+
+
+
+
+
+end
