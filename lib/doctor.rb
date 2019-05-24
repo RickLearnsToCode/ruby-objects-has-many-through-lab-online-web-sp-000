86cb93747a53ee51d24952b0526c629bb8c69aed
Rick Rosetti <rick.rosetti@gmail.com>
2019-05-24 04:05:04 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,8 +13,19 @@ class Doctor
   end
 
   def new_appointment(patient, date)
-    Appointment.new(date, patient, doctor)
+    Appointment.new(date, patient, self)
   end
+
+  def appointments
+    Appointment.all.select {|appt| appt.doctor == self}
+  end
+
+  def patients
+    appointments.map {|appt| appt.patient}
+  end
+
+
+
 
 
 
